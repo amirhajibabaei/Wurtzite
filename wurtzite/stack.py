2f12e829ef960e4bb89d698a709f2c6592cf069c
Amir Hajibabaei <ah2281@wildcat>
2023-01-03 18:58:48 +0000

    repr Stack

--- wurtzite/stack.py
+++ wurtzite/stack.py
@@ -11,32 +11,32 @@ class Stack:
     def __init__(self, symbols, layers, spacings, vacuum=20.0):
         assert len(symbols) == len(layers) == len(spacings) + 1
         self._symbols = symbols
-        self.layers = layers
-        self.spacings = spacings
-        self.vacuum = vacuum
+        self._layers = layers
+        self._spacings = spacings
+        self._vacuum = vacuum
 
     @property
     def cell(self):
-        cells = [layer.xy_cell() for layer in self.layers]
+        cells = [layer.xy_cell() for layer in self._layers]
         assert np.allclose(np.stack(cells) - cells[0], 0)
         cell_xy = np.c_[cells[0], [0, 0]]
-        cell_z = sum(self.spacings) + 2 * self.vacuum
+        cell_z = sum(self._spacings) + 2 * self._vacuum
         cell = np.r_[cell_xy, [[0, 0, cell_z]]]
         return cell
 
     @property
-    def symbols(self):
+    def chemical_symbols(self):
         symbols = []
-        for layer, symbol in zip(self.layers, self._symbols):
+        for layer, symbol in zip(self._layers, self._symbols):
             symbols.extend(layer.n * [symbol])
         return symbols
 
     @property
     def positions(self):
         xy, z = [], []
-        spacings = [*self.spacings, 0]
-        _z = self.vacuum
-        for layer, delta in zip(self.layers, spacings):
+        spacings = [*self._spacings, 0]
+        _z = self._vacuum
+        for layer, delta in zip(self._layers, spacings):
             xy.append(layer.xy_positions())
             z.append(layer.n * [_z])
             _z += delta
@@ -45,9 +45,21 @@ class Stack:
 
     def as_atoms(self):
         atoms = Atoms(
-            symbols=self.symbols, positions=self.positions, cell=self.cell, pbc=True
+            symbols=self.chemical_symbols,
+            positions=self.positions,
+            cell=self.cell,
+            pbc=True,
         )
         return atoms
+
+    def __repr__(self):
+        spacings = [*self._spacings, 0]
+        _z = self._vacuum
+        rep = [f"{self.__class__.__name__}:"]
+        for layer, symbol, delta in zip(self._layers, self._symbols, spacings):
+            _z += delta
+            rep.append(f"{layer.n:>6} x {symbol:<2} at z = {_z}")
+        return "\n".join(rep)
 
 
 class _WurtZite(Stack):
