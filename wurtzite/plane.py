022efddea56a8c355ee989141c6d3a99b98dff67
Amir Hajibabaei <ah2281@wildcat>
2023-01-04 19:14:10 +0000

    refactor

---
+++ wurtzite/plane.py
@@ -0,0 +1,160 @@
+# +
+import abc
+from collections import Counter
+from collections.abc import Sequence
+
+import numpy as np
+from ase.data import atomic_numbers
+
+
+class Plane:
+    @abc.abstractmethod
+    def xy_positions(self):
+        ...
+
+    @abc.abstractmethod
+    def xy_cell(self):
+        ...
+
+    def __len__(self):
+        return len(self.xy_positions())
+
+    def get_positions(self, z):
+        xy = self.xy_positions()
+        z = np.full(xy.shape[0], z)
+        return np.c_[xy, z]
+
+    def translate(self, tr):
+        return ShiftedPlane(self, tr)
+
+
+class CustomPlane(Plane):
+    def __init__(self, xy_positions, xy_cell):
+        self._pos = xy_positions
+        self._cell = xy_cell
+
+    def xy_positions(self):
+        return self._pos
+
+    def xy_cell(self):
+        return self._cell
+
+
+class LatticePlane(Plane):
+    def __init__(self, vectors, basis, nxy):
+        self._vectors = np.asarray(vectors)
+        self._basis = np.asarray(basis)
+        self._nxy = nxy
+        self._pos = None
+        self._cell = None
+
+    def xy_positions(self):
+        if self._pos is None:
+            nx, ny = self._nxy
+            p = np.mgrid[0:nx, 0:ny].reshape(2, -1).T
+            q = (p[:, None] + self._basis).reshape(-1, 2)
+            self._pos = (q[..., None] * self._vectors).sum(axis=-2)
+        return self._pos
+
+    def xy_cell(self):
+        if self._cell is None:
+            nxy = np.asarray(self._nxy).reshape(2, 1)
+            self._cell = nxy * self._vectors
+        return self._cell
+
+
+class CubicPlane(LatticePlane):
+    def __init__(self, a, nxy):
+        uc = (
+            np.array(
+                [
+                    [1, 0],  #
+                    [0, 1],  #
+                ]
+            )
+            * a
+        )
+
+        b = np.array([[0, 0]])
+        super().__init__(uc, b, nxy)
+
+
+class HexagonalPlane(LatticePlane):
+    def __init__(self, a, nxy):
+        uc = (
+            np.array(
+                [
+                    [1, 0],  #
+                    [1 / 2, np.sqrt(3) / 2],  #
+                ]
+            )
+            * a
+        )
+        b = np.array([[0, 0]])
+        super().__init__(uc, b, nxy)
+
+
+class HexagonalPlaneCC(LatticePlane):
+    def __init__(self, a, nxy):
+        uc = (
+            np.array(
+                [
+                    [1, 0],  #
+                    [0, np.sqrt(3)],  #
+                ]
+            )
+            * a
+        )
+        b = np.array([[0, 0], [0.5, 0.5]])
+        super().__init__(uc, b, nxy)
+
+
+class ShiftedPlane(Plane):
+    def __init__(self, plane, tr):
+        self._plane = plane
+        self._tr = np.asarray(tr)
+        self._pos = None
+
+    def xy_positions(self):
+        if self._pos is None:
+            self._pos = self._plane.xy_positions() + self._tr
+        return self._pos
+
+    def xy_cell(self):
+        return self._plane.xy_cell()
+
+
+class AtomicPlane:
+    def __init__(self, plane, atoms="X"):
+
+        if type(atoms) == str:
+            assert atoms in atomic_numbers
+        elif isinstance(atoms, Sequence):
+            assert len(atoms) == len(plane)
+            unknown = [a for a in set(atoms) if a not in atomic_numbers]
+            assert len(unknown) == 0
+        else:
+            raise RuntimeError("Invalid atoms!")
+
+        self._plane = plane
+        self._atoms = atoms
+
+    @property
+    def atoms(self):
+        if type(self._atoms) == str:
+            return len(self._plane) * [self._atoms]
+        else:
+            return self._atoms
+
+    @property
+    def xy_cell(self):
+        return self._plane.xy_cell()
+
+    def get_positions(self, z):
+        return self._plane.get_positions(z)
+
+    def replace_atoms(self, atoms):
+        return AtomicPlane(self._plane, atoms)
+
+    def __repr__(self):
+        return f"{Counter(self.atoms)}"
--- wurtzite/stack.py
+++ wurtzite/stack.py
@@ -4,106 +4,65 @@ import abc
 import numpy as np
 from ase import Atoms
 
-from .layers import Hexagonal, HexagonalCC, Shifted
+from .plane import AtomicPlane, HexagonalPlane, HexagonalPlaneCC
 
 
 class Stack:
-    def __init__(self, symbols, layers, spacings, vacuum=20.0):
-        assert len(symbols) == len(layers) == len(spacings) + 1
+    def __init__(self, atomic_planes, spacings, vacuum=20.0):
+        # assertions
+        assert len(atomic_planes) == len(spacings) + 1
         assert all([s >= 0 for s in spacings])
-        self._symbols = symbols
-        self._layers = layers
+        cells = np.stack([p.xy_cell for p in atomic_planes])
+        assert np.allclose(cells - cells[0], 0), "all xy-cells must be identical"
+
+        # init
+        self._atomic_planes = atomic_planes
         self._spacings = spacings
         self._vacuum = vacuum
 
-    def set_layer(self, index, *, symbol=None, spacing=None):
-        assert index >= 0 and index < self.number_of_layers, "index out of range"
-        if symbol is not None:
-            self._symbols[index] = symbol
-        if spacing is not None:
-            assert index != 0, "spacing can't be defined for the first layer"
-            self._spacings[index - 1] = spacing
-
-    def insert_layer(self, index, symbol, layer, spacing):
-        """
-        spacing is defined as:
-            if index == 0:
-                the spacing between the inserted and the next layer
-            otherwise:
-                the spacing between the inserted and the previous layer
-        """
-        assert index >= 0 and index <= self.number_of_layers, "index out of range"
-        self._symbols.insert(index, symbol)
-        self._layers.insert(index, layer)
-        if index == 0:
-            self._spacings.insert(0, spacing)
-        else:
-            self._spacings.insert(index - 1, spacing)
-
-    @property
-    def number_of_layers(self):
-        return len(self._layers)
-
-    @property
-    def number_of_atoms_in_layers(self):
-        return [layer.num_sites for layer in self._layers]
-
-    @property
-    def number_of_atoms(self):
-        return sum(self.number_of_atoms_in_layers)
-
     @property
     def cell(self):
-        cells = [layer.xy_cell() for layer in self._layers]
-        assert np.allclose(np.stack(cells) - cells[0], 0)
-        cell_xy = np.c_[cells[0], [0, 0]]
-        cell_z = sum(self._spacings) + 2 * self._vacuum
-        cell = np.r_[cell_xy, [[0, 0, cell_z]]]
+        xy = self._atomic_planes[0].xy_cell
+        z = sum(self._spacings) + 2 * self._vacuum
+        _xyz = np.c_[xy, [0, 0]]
+        cell = np.r_[_xyz, [[0, 0, z]]]
         return cell
 
     @property
-    def _z(self):
+    def z_positions(self):
         z = [self._vacuum]
         for delta in self._spacings:
             z.append(z[-1] + delta)
         return z
 
     @property
-    def chemical_symbols(self):
-        symbols = []
-        for layer, symbol in zip(self._layers, self._symbols):
-            symbols.extend(layer.num_sites * [symbol])
-        return symbols
+    def positions(self):
+        xyz = []
+        for p, z in zip(self._atomic_planes, self.z_positions):
+            xyz.append(p.get_positions(z))
+        return np.concatenate(xyz)
 
     @property
-    def positions(self):
-        xy, z = [], []
-        for layer, _z in zip(self._layers, self._z):
-            xy.append(layer.xy_positions())
-            z.append(layer.num_sites * [_z])
-        xyz = np.c_[np.concatenate(xy), np.concatenate(z)]
-        return xyz
+    def atoms(self):
+        symbols = []
+        for p in self._atomic_planes:
+            symbols.extend(p.atoms)
+        return symbols
 
-    def as_atoms(self):
+    def as_ase_atoms(self):
         atoms = Atoms(
-            symbols=self.chemical_symbols,
+            symbols=self.atoms,
             positions=self.positions,
             cell=self.cell,
             pbc=True,
         )
         return atoms
 
-    def __repr__(self):
-        rep = [f"{self.__class__.__name__}:"]
-        spacings = ["", *[f"(+ {delta:0.6f})" for delta in self._spacings]]
-        for index, (layer, symbol, z, delta) in enumerate(
-            zip(self._layers, self._symbols, self._z, spacings)
-        ):
-            rep.append(
-                f"{index:>6} -> {layer.num_sites:>4} "
-                f"x {symbol:<2} at z = {z:10.6f}  {delta}"
-            )
-        return "\n".join(rep)
+    def __getitem__(self, index):
+        return self._atomic_planes[index]
+
+    def __setitem__(self, index, value):
+        self._atomic_planes[index] = value
 
 
 class _WurtZite(Stack):
@@ -112,28 +71,32 @@ class _WurtZite(Stack):
     ):
         nx, ny, nz = nxyz
         A, B = symbols
-        a, b = self._hexagonal_layers(cell_a, nx, ny)
-        layers = nz * [a, b, b, a]
-        symbols = nz * [A, B, A, B]
+        a, b = self._hexagonal(cell_a, (nx, ny))
+        atomic_planes = nz * [
+            AtomicPlane(a, A),
+            AtomicPlane(b, B),
+            AtomicPlane(b, A),
+            AtomicPlane(a, B),
+        ]
         spacings = (nz * [cell_z1, cell_z2, cell_z1, cell_z2])[:-1]
         if surf_z1 is not None:
             spacings[0], spacings[-1] = surf_z1
-        super().__init__(symbols, layers, spacings, vacuum=vacuum)
+        super().__init__(atomic_planes, spacings, vacuum=vacuum)
 
     @abc.abstractmethod
-    def _hexagonal_layers(self, cell_a, nx, ny):
+    def _hexagonal(self, cell_a, nxy):
         ...
 
 
 class WurtZiteCC(_WurtZite):
-    def _hexagonal_layers(self, cell_a, nx, ny):
-        a = HexagonalCC(cell_a, (nx, ny))
-        b = Shifted(a, [0, cell_a / np.sqrt(3)])
+    def _hexagonal(self, cell_a, nxy):
+        a = HexagonalPlaneCC(cell_a, nxy)
+        b = a.translate([0, cell_a / np.sqrt(3)])
         return a, b
 
 
 class WurtZite(_WurtZite):
-    def _hexagonal_layers(self, cell_a, nx, ny):
-        a = Hexagonal(cell_a, (nx, ny))
-        b = Shifted(a, [cell_a, cell_a / np.sqrt(3)])
+    def _hexagonal(self, cell_a, nxy):
+        a = HexagonalPlane(cell_a, nxy)
+        b = a.translate([cell_a, cell_a / np.sqrt(3)])
         return a, b
